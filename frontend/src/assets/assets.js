464736f21cab65d0cfbe31321e7083ec98825960
Krishvora22 <krishvora22@gmail.com>
2025-11-14 22:17:46 +0530

    cloud update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -55,9 +55,9 @@ import p_img51 from './p_img51.png'
 import p_img52 from './p_img52.png'
 
 import logo from './logo.png'
-import hero_img from "./Hero_img.png";        // image 1
-import hero_img_1 from "./Hero_img_1.png";    // image 2
-import hero_img_2 from "./Hero_img_2.png";    // image 3
+import hero_img from "./hero_img.png";
+import hero_img_1 from "./Hero_img_1.png";
+import hero_img_2 from "./Hero_img_2.png";
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
 import dropdown_icon from './dropdown_icon.png'
@@ -96,8 +96,7 @@ export const assets = {
     cross_icon,
     hero_img,
   hero_img_1,
-  hero_img_2
-
+  hero_img_2,
 }
 
 export const products = [
